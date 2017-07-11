bd1e7d1e77cf23a9cd4362076196ac40c0bf6b4c
charles.howe <charles.p.howe@usace.army.mil>
2017-07-11 12:06:40 -0400

    improving error handling and passing control to the connection release

--- index.js
+++ index.js
@@ -11,36 +11,33 @@ var printErrorInfo =  function(obj){
   }
 };
 
+function doRelease(connection){
+  connection.release(
+    function (err) {
+      if(err){
+        console.error(err.message);
+      }
+    }
+  );
+}
+
+function doClose(connection, resultSet) {
+  resultSet.close(
+    function(err){
+      if (err) { 
+        console.error(err.message); 
+      }
+      doRelease(connection);
+    }
+  );
+}
+
 module.exports = {
   //name on right side of : gives it a name that can be called within the scope of itself
-  fetchRowsFromRS : function fetchRowsFromRS(connection, res, next, resultSet, numRows, dataSet, helper) {
+  fetchRowsFromRS : function fetchRowsFromRS(connection, res, resultSet, numRows, dataSet, helper) {
     var parentRes = res;
     var useLoader = (helper.queryOpts && helper.queryOpts.useLoader);
     //Close the resultset out after it has been looped through
-    function doClose(connection, resultSet, next) {
-      resultSet.close(
-        function(err){
-          if (err) { 
-            console.error(err.message); 
-          }
-
-          if(connection){
-            connection.release(
-              function(err){
-                if (err) { 
-                  console.error(err.message); 
-                  next(err);
-                } else {
-                  next();
-                }
-              }
-            );
-          } else {
-            next();
-          }
-        }
-      );
-    }
     
     resultSet.getRows( // get numRows rows
        numRows,
@@ -48,6 +45,14 @@ module.exports = {
         if (err) {
           console.error("error in resultSet");
           console.error(err);
+          parentRes.writeHead(500, {'Content-Type': 'application/json'});
+          parentRes.end(
+            JSON.stringify({
+              status: 500,
+              message: "Error with fetching result set",
+              detailed_message: err.message
+            })
+          );
           doClose(connection, resultSet); // always close the result set
         } else if (rows.length === 0 || (!helper.validReq && helper.rsRunCount > 0)) {    // no rows, or no more rows
           try {
@@ -65,7 +70,7 @@ module.exports = {
           try{
             dataSet = helper.loadFn(dataSet,rows);
        
-            fetchRowsFromRS(connection, res, next, resultSet, numRows, dataSet, helper);
+            fetchRowsFromRS(connection, res, resultSet, numRows, dataSet, helper);
           } catch (e){
             doClose(connection, resultSet);
           }
@@ -99,41 +104,47 @@ module.exports = {
           console.error('Path: ' + req.route.path);
           console.error('Params: ' + printErrorInfo(req.query));
           console.error(err.message);
-          if(connection){
-            connection.release(function() {
-                next(err);
-            });
-          } else {
-            next(err);
-          }
+
+          parentRes.writeHead(500, {'Content-Type': 'application/json'});
+          parentRes.end(
+            JSON.stringify({
+              status: 500,
+              message: "Error connecting to DB",
+              detailed_message: err.message
+            })
+          );
+          return;
         }
 
         if(typeof data.query === 'string'){
           connection.execute(
-            data.query
-            ,data.params
-            ,data.queryOpts
-            ,function (err, result) {
+            data.query,
+            data.params,
+            data.queryOpts,
+            function (err, result) {
               if (err) {
                 console.error('Error in querying or fetching data');
                 console.error('Path: ' + req.route.path);
                 console.error('Params: ' + printErrorInfo(req.query));
                 console.error(err.message);
-                connection.release(
-                  function () {
-                      next(err);
-                  }
+                parentRes.writeHead(500, {'Content-Type': 'application/json'});
+                parentRes.end(
+                  JSON.stringify({
+                    status: 500,
+                    message: "Error querying DB",
+                    detailed_message: err.message
+                  })
                 );
+                doRelease(connection);
               } else if (result){
                 if (isResultSet){
                   var dataSet = {};
-                  self.fetchRowsFromRS(connection, res, next, result.resultSet, data.queryOpts.prefetchRows, dataSet, data);
+                  self.fetchRowsFromRS(connection, res, result.resultSet, data.queryOpts.prefetchRows, dataSet, data);
                 } else {
                   connection.release(
                     function (err) {
                       if (err) {
                         console.error(err.message);
-                        next(err);
                       } else { 
                         if (useLoader){
                           //assume that the socket is within the loadFn
@@ -152,11 +163,16 @@ module.exports = {
         } else {
           console.error('Query String not provided');
           console.error('Path: ' + req.route.path);
-          connection.release(
-            function () {
-                next();
-            }
+          console.error(err.message);
+          parentRes.writeHead(500, {'Content-Type': 'application/json'});
+          parentRes.end(
+            JSON.stringify({
+              status: 500,
+              message: "Error querying DB",
+              detailed_message: err.message
+            })
           );
+          doRelease(connection);
         }
       }
     );
